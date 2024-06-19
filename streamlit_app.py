4b8819e4902617cdd35f4dad30fbebaf2c00db7c
jkrun67521 <lig67521@gmail.com>
2024-06-19 19:24:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -28,6 +29,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)    
         
     st.write(ingredients_string)
 
@@ -44,27 +47,6 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered, '+ name_on_order +'!', icon="✅")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-# Write directly to the app
-# st.title("Customize Your Smoothie! :cup_with_straw:")
-# st.write(
-#    """Replace this example with your own code!
-#    **And if you're new to Streamlit,** check
-#    out our easy-to-follow guides at
-#    [docs.streamlit.io](https://docs.streamlit.io).
-#    """
-# )
 
-# option = st.selectbox(
-#    "What is your favorite fruit?",
-#    ("Banana", "Strawberries", "Peaches"))
 
-# st.write("Your favorite fruit is:", option)
-
-# session = get_active_session()
-# my_dataframe = session.table("smoothies.public.fruit_options")
-# st.dataframe(data=my_dataframe, use_container_width=True)
